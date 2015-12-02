3a15b111868f0c6307ee7de3d110a20df397bba2
Mimi Schatz <mcschatz@gmail.com>
2015-12-02 16:20:25 -0700

    added validation for uniqueness on idea.body

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,6 @@
 class Idea < ActiveRecord::Base
   validates :title, presence: true
-  validates :body, presence: true
+  validates :body, presence: true, uniqueness: true
 
   enum quality: %w(Swill Plausible Genius)
 end
