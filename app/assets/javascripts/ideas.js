4f7ccb48c72beb5b0402d7233828056d359787e6
Mimi Schatz <mcschatz@gmail.com>
2015-12-01 21:15:25 -0700

    can delete an idea

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,27 +1,32 @@
 $(document).ready(function () {
-  getIdeas()
-  createIdeas()
+  getIdeas();
+  createIdea();
+  deleteIdea();
 });
 
-function getIdeas() {
-  $.ajax({
-    type: 'GET',
-    url: '/api/v1/ideas',
-    success: function(response) {
-      var ideaElements = $.map (response, function (idea, index) {
-        var ideaProperties = $(".ideas-list").prepend(
-          "<h3>" + idea.title + "</h3>"
-          + "<h5>" + idea.body + "</h5>"
-          + "<h5>" + idea.quality + "</h5>"
-          + "<input class='btn btn-default' id='delete-idea' type='button' name='submit' value='Delete Idea'>"
-        )
-        return ideaProperties;
-      });
-    }
+function getIdeas(){
+  $.getJSON('/api/v1/ideas', function(data) {
+    $.each(data, function(index, idea){
+      renderIdeas(idea)
+    })
   });
 };
 
-function createIdeas(){
+function renderIdeas(idea) {
+  $('#ideas-list').prepend(
+    "<li class='collection-item idea' data-id='" + idea.id
+    + "'><div class='row'>"
+    + "<p>Title: "
+    + idea.title
+    + "</p><p>Description: "
+    + idea.body
+    + "</p><p>Quality: "
+    + idea.quality
+    + "</div><i class='material-icons' id='delete-idea'>delete</i></li>"
+  )
+};
+
+function createIdea(){
   $('#create-idea').on('click', function(){
     var ideaTitle = $('#idea-title').val();
     var ideaBody = $('#idea-body').val();
@@ -39,10 +44,30 @@ function createIdeas(){
       type: 'POST',
       url: '/api/v1/ideas.json',
       data: ideaParams,
-      success: function(){
-        getIdeas()
+      success: function(idea){
+        renderIdeas(idea)
       }
     });
     $('form').trigger('reset');
+  })
+}
+
+function deleteIdea() {
+  $('#ideas-list').delegate('#delete-idea', 'click', function(){
+    var $idea = $(this).closest('.idea')
+
+    $.ajax({
+      type: 'DELETE',
+      url:  '/api/v1/ideas/'
+      + $idea.attr('data-id')
+      + '.json',
+      success: function(){
+        $idea.remove()
+      },
+      error: function(){
+        $idea.remove()
+        console.log('This Idea has already deleted')
+      }
+    })
   })
 }
