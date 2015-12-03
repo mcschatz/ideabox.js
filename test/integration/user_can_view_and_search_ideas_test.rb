c540a0263986aa4dcd92fd98cf1f54ca90263a8b
Mimi Schatz <mcschatz@gmail.com>
2015-12-03 07:52:34 -0700

    add user search tests

--- test/integration/user_can_view_and_search_ideas_test.rb
+++ test/integration/user_can_view_and_search_ideas_test.rb
@@ -11,4 +11,28 @@ class UserCanViewAndSearchIdeasTest < ActionDispatch::IntegrationTest
 
     assert page.has_content?("ThoughtPour")
   end
+
+  test "user can search by title" do
+    visit root_path
+    find("#search").click
+    fill_in("search", with: "First")
+    assert page.has_content?("First One")
+    refute page.has_content?("Silly")
+  end
+
+  test "user can search by body" do
+    visit root_path
+    find("#search").click
+    fill_in("search", with: "Happy")
+    assert page.has_content?("Plausible")
+    refute page.has_content?("Silly")
+  end
+
+  test "user can search by quality" do
+    visit root_path
+    find("#search").click
+    fill_in("search", with: "Swill")
+    assert page.has_content?("FINISHED")
+    refute page.has_content?("Happy")
+  end
 end
